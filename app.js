55473843cc20198ad353ea119b84d478c88da923
frishlin <frishlin@gmail.com>
2017-10-24 14:42:55 -0500

    ejercicio comparar

---
+++ app.js
@@ -0,0 +1,7 @@
+var comparacion = function(array1, array2){
+  document.write("funcion de prueba");
+}
+
+var arreglo1=[1,2,3,4,5,6,7,8,9,0];
+var arreglo2=[1,2,3,4,5,6,7,8,9,0];
+comparacion(arreglo1,arreglo2);
